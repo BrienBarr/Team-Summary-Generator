35e97c3f5a890b3cde086bb62c1562a8e06a0c12
BrienB <barrdom@hotmail.com>
2021-01-24 11:21:07 -0500

    removed console logs and added comments

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
+// Declare Node.js dependencies 
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./lib/htmlRenderer");
 
+// Declare the output directory and path for the team.html file created by the app
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
+// Declare an empty array to store the employees added from user input
 let employees = [];
+
+// Declare an undefined variable to store the employee to be added from current user prompt
 let employee;
 
+// Declare the first set of questions to add the team manager's info
 const questions = [
     {
         type: 'input',
@@ -51,6 +52,7 @@ const questions = [
     }
 ];
 
+// Declare the set of questions to gather an engineer's info
 const engineerQuestions = [
     {
         type: 'input',
@@ -85,6 +87,7 @@ const engineerQuestions = [
     }
 ]
 
+// Declare the set of questions to gather an intern's info
 const internQuestions = [
     {
         type: 'input',
@@ -119,35 +122,39 @@ const internQuestions = [
     }
 ]
 
+// Function to prompt the user with questions in the command-line and add employee to the employees array
 const userPrompt = (questions) => {
     inquirer
         .prompt(
             questions
         )
+        // Promise to perform after user has been prompted with questions
         .then((response) => {
-            // console.log(response);
+
+            // Switch statement to create a new Engineer, Intern, or Manager depending upon 
+            // the set of questions asked and push that employee to the employees array
             switch (questions){
                 case engineerQuestions:
                     response.role = "Engineer";
                     employee = new Engineer (response.name, response.id, response.email, response.github);
                     employees.push(employee);
-                    // console.log(employees);
                     break;
 
                 case internQuestions:
                     response.role = "Intern";
                     employee = new Intern (response.name, response.id, response.email, response.school);
                     employees.push(employee);
-                    // console.log(employees);
                     break;
 
                 default: 
                     response.role = "Manager";
                     employee = new Manager (response.name, response.id, response.email, response.officeNumber);
                     employees.push(employee);
-                    // console.log(employees);
             }
             
+            // Switch statement to repeat user prompt via command-line with the appropriate questions
+            // depending upon if the user wants to add another employee and 
+            // what type of employee they want to add to the team
             switch (response.add){
                 case "Engineer":
                     userPrompt(engineerQuestions);
@@ -157,35 +164,17 @@ const userPrompt = (questions) => {
                     userPrompt(internQuestions);
                     break;
 
+                // When the user is done adding employees, use the render module to create the html file
+                // and output it to the output path
                 default:
-                    // console.log(employees);
                     const team = render(employees);
-                    // console.log(team);
                     fs.writeFile(outputPath, team, err => err ? err : "File Successfully Written!");
-                    //return team;
             }
 
         })
 };
 
+// Inital call of the userPrompt function
 userPrompt(questions);
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
